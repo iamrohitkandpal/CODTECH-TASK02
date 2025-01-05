c6b9407bd33337e1a8b1abaec0982728f45b7e92
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-01-05 23:54:35 +0530

    All Mongoose Schemas created and connection with the MongoDB Established

---
+++ server/models/blog.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+    },
+    category: {
+      type: String,
+      default: "general",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    visit: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Blog", blogSchema);
---
+++ server/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkUserId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    img: {
+      type: String,
+    },
+    savedBlogs: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
