c172eb438c67745996bd9b527e006d2f990d530d
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-01-09 23:58:42 +0530

    Some changes done in the blog and user routes some new routes and rectifications

--- server/routes/blog.routes.js
+++ server/routes/blog.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { createBlog, deleteBlog, getBlog, getBlogs, uploadAuth } from '../controllers/blog.controller.js';
+import { createBlog, deleteBlog, getBlog, getBlogs, uploadAuth, featurePost } from '../controllers/blog.controller.js';
 
 const router = express.Router();
 
 router.get("/upload-auth", uploadAuth);
+
 router.get("/", getBlogs);
 router.get("/:slug", getBlog);
 router.post("/", createBlog);
-router.delete("/delete/:slug", deleteBlog);
+router.delete("/delete/:id", deleteBlog);
+router.patch("feature", featurePost);
 
 export default router;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import { getSavedBlogs, saveBlog } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get("kl", (req, res) => {
-    
-})
+router.get("/saved", getSavedBlogs);
+router.patch("/save", saveBlog);
 
 export default router;
